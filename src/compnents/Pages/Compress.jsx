78cde24bf82183fd190dfdc981875f0490e48bad
ADEDEWE BOLUWATIFE <lilblessingjnr@gmail.com>
2023-06-05 15:17:38 -0400

    now bulidng with component appproach. welcome to react mf

--- src/compnents/Pages/Compress.jsx
+++ src/compnents/Pages/Compress.jsx
@@ -1,44 +1,17 @@
 import React from 'react'
+import Input from './AddMember/Input'
+import SearchBoards from './AddMember/SearchBoards'
 
 export default function Compress() {
   return (
      <main id="mainContentCont">
       <header id="headersHide">
-        <div id="member-name">
-          <form id="survey-form">
-            <label
-              >Trello-name:
-              <input
-              className='inputValue'
-                type="text"
-                id="resultoo"
-                placeholder="Enter Email"
-               />
-            </label>
-          </form>
-        </div>
+      <Input/>
+      <SearchBoards/>
+
         <h1 id="signin">Select Boards To Add Member To</h1>
 
-        <section className='searchSection'>
-          <input
-            id="search"
-            type="text"
-            placeholder="Search Boards ..." />
-        </section>
-
-        <section id="selective-btn">
-          <section className="selecting-btn-cont">
-            <button id="select-all">Select All</button>
-            <button id="clear-select">Deselect All</button>
-          </section>
-
-          <div style="margin-top: 8px; margin-bottom: 10px">
-            <button id="deleting-btn">Add Member</button>
-            <div id="para"></div>
-            <div
-              id="email-error2"></div>
-          </div>
-        </section>
+       
       </header>
 
       <div id="success"></div>
