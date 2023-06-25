8c6798db8875883102aafb2e53a0f098850d9fa3
ADEDEWE BOLUWATIFE <lilblessingjnr@gmail.com>
2023-06-25 13:12:25 -0400

    yeah

--- Frontend/src/compnents/Pages/Delete.jsx
+++ Frontend/src/compnents/Pages/Delete.jsx
@@ -3,8 +3,8 @@ import Input from './BasicSectionLayout/Input'
 import SearchBoards from './BasicSectionLayout/SearchBoards'
 import SelectAll from './BasicSectionLayout/SelectAll'
 import ProgressBar from '../ProgressBar/ProgressBar'
-import FetchData from '../../JS functions/fetchData';
-import DeleteMemberFromBoard from '../../JS functions/DeleteFromBoard';
+import FetchData from '/root/Trello-Project-React/Frontend/src/JS functions/fetchData';
+import DeleteMemberFromBoard from '/root/Trello-Project-React/Frontend/src/JS functions/DeleteFromBoard';
 
 
 export default function Delete() {
