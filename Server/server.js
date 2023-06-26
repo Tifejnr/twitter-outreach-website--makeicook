11feea12a68db3f04306ce9c2d00d8aeb39fe54d
ADEDEWE BOLUWATIFE <lilblessingjnr@gmail.com>
2023-06-25 20:11:24 -0400

    again

--- Server/server.js
+++ Server/server.js
@@ -18,13 +18,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "../../Frontend/dist")));
 
 app.get("/*", function (req, res) {
-  res.sendFile(
-    path.join(
-      __dirname,
-      "../../../Trello-Project-React/Frontend/dist",
-      "/root/Trello-Project-React/Frontend/dist/index.html"
-    )
-  );
+  res.sendFile(path.join(__dirname, "../../Frontend/dist", "index.html"));
 });
 
 app.set("view engine", "ejs");
