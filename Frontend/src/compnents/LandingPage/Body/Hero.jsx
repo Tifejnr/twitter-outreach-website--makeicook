82a170b3b5e48937167387817d4bef95830e5fb8
ADEDEWE BOLUWATIFE <lilblessingjnr@gmail.com>
2023-06-26 05:46:07 -0400

    still on the design. great work

--- Frontend/src/compnents/LandingPage/Body/Hero.jsx
+++ Frontend/src/compnents/LandingPage/Body/Hero.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 export default function Hero() {
   return (
    <section className='hero-container'>
-    <h1>Easily Add a Member to Multiple Boards on Trello!</h1>
+    <h1>Add Member to Multiple Boards on Trello!</h1>
+
+    <h3>All-in-one tool for addition and removal of members on Trello</h3>
+
+    <a href="" className='oauth-button'><h2>Continue with Trello</h2></a>
    </section>
   )
 }
