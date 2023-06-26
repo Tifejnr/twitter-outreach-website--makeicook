445e33278ba82307f056d10f48b9cff6a8208812
ADEDEWE BOLUWATIFE <lilblessingjnr@gmail.com>
2023-06-25 23:39:30 -0400

    building landing page fo the app

---
+++ Frontend/src/compnents/Navbar/LandingPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function LandingPage() {
+  return (
+   <nav className='nav'>
+
+    <a href='/' className='site-title'>Site Name</a>
+
+    <NavItems/>
+
+   </nav>  
+   )
+}
---
+++ Frontend/src/compnents/Pages/LandingPage/Navs.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Link, useMatch, useResolvedPath} from "react-router-dom"
+
+export default function NavItems() {
+  return (
+    <ul className='nav-items-cont'>
+        <CustomLink to="/platforms">Platforms</CustomLink> 
+        <CustomLink to="/features">Features</CustomLink> 
+        <CustomLink to="/use-cases">Use cases</CustomLink> 
+        <CustomLink to='/help'>Help</CustomLink> 
+        <CustomLink to='/pricing'>Pricing</CustomLink> 
+    </ul>
+  )
+}
+
+
+//Nav program that is active function
+const CustomLink= ({to, children, ...props}) => {
+ const resolvedPath = useResolvedPath(to);
+
+ const isActive =  useMatch({path: resolvedPath.pathname, end:true})
+
+ return (
+  <li className={isActive && "active"}>
+    <Link to={to} {...props}>
+      {children}
+    </Link>
+
+  </li>
+ )
+}
+
+
