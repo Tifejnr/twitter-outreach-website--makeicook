30f9ed800bece3babc9415e0cff6b0a53f6958de
ADEDEWE BOLUWATIFE <lilblessingjnr@gmail.com>
2023-06-26 23:42:44 -0400

    way better, now starting with mobile first approach for styling

--- Frontend/src/compnents/LandingPage/Body/Hero.jsx
+++ Frontend/src/compnents/LandingPage/Body/Hero.jsx
@@ -6,15 +6,17 @@ export default function Hero() {
   return (
 
     <section className='hero-container'> 
+     <div className='hero-inner-container'>
       <h1>Add Trello Member to Multiple Boards at Once</h1>
       <h3>Saves time that can be utilized for more productive activities</h3>
 
         <a href={oauthUrl} className='oauth-button'>
           <section>
           <img src= {trelloIcon} className='trello-icon' />
-          <h2>Continue with Trello</h2>
+          <h2>Get Started with Trello</h2>
           </section>
         </a>
+        </div>
     </section>
   )
 }
