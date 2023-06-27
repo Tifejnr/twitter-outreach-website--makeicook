a4e578f460635dba1482f1f884adc192992ce636
ADEDEWE BOLUWATIFE <lilblessingjnr@gmail.com>
2023-06-26 22:59:58 -0400

    design improved

--- Frontend/src/compnents/LandingPage/Body/Hero.jsx
+++ Frontend/src/compnents/LandingPage/Body/Hero.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import trelloIcon from "../../../assets/trello-icon.svg"
+const oauthUrl= "https://collabfortrello.com/authorize"
 
 export default function Hero() {
   return (
-   <section className='hero-container'>
-    <h1>Add Member to Multiple Boards on Trello!</h1>
 
-    <h3>All-in-one tool for addition and removal of members on Trello</h3>
+    <section className='hero-container'> 
+      <h1>Add Trello Member to Multiple Boards at Once</h1>
+      <h3>Saves time that can be utilized for more productive activities</h3>
 
-    <a href="" className='oauth-button'>
-      <section>
-       <  img src= {trelloIcon} className='trello-icon' />
-       <h2>Continue with Trello</h2>
-      </section>
-    </a>
-   </section>
+        <a href={oauthUrl} className='oauth-button'>
+          <section>
+          <img src= {trelloIcon} className='trello-icon' />
+          <h2>Continue with Trello</h2>
+          </section>
+        </a>
+    </section>
   )
 }
