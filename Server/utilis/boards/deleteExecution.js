5fceec6f88c046862332cb4546135fee052b590b
ADEDEWE BOLUWATIFE <lilblessingjnr@gmail.com>
2023-07-11 16:46:00 +0200

    adding token to functionlity routes

--- Server/utilis/boards/deleteExecution.js
+++ Server/utilis/boards/deleteExecution.js
@@ -4,9 +4,9 @@ const { getKeys } = require("../../envKeys/allKeys");
 //fetching env variables
 const keysObj = getKeys();
 const key = keysObj.CLIENT_SECRET_KEY;
-const token = keysObj.ACCESS_TOKEN_SECRET;
+// const token = keysObj.ACCESS_TOKEN_SECRET;
 
-async function deleteExecution(boardId, memberId) {
+async function deleteExecution(boardId, memberId, token) {
   const boardDeleteUrl = `
   https://api.trello.com/1/boards/${boardId}/members/${memberId}?key=${key}&token=${token}`;
 
