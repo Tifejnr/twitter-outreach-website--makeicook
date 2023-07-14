f8f6bd95eabb3cc9d47f481bcff94c20b2a0fd23
ADEDEWE BOLUWATIFE <lilblessingjnr@gmail.com>
2023-07-14 19:06:17 +0200

    button n toggle between select all and unselect all fixed with conditional rendering

--- Frontend/src/compnents/Pages/BasicSectionLayout/SelectAll.jsx
+++ Frontend/src/compnents/Pages/BasicSectionLayout/SelectAll.jsx
@@ -1,16 +1,29 @@
-import React from 'react'
-
+import React, {useState} from 'react'
 import CheckAll from '../../../JS functions/Utilis/Selections.jsx/CheckAll'
 import UncheckAll from '../../../JS functions/Utilis/Selections.jsx/UncheckAll'
 
 export default function SelectAll(props) {
+  const [isAllChecked, setIsAllChecked] =  useState(false)
   return (
        <section className='selectionCont' id="selective-btn">
 
           <h1>{props.selectInstructionText}</h1>
-          <section className="selecting-btn-cont">
-            <button id="select-all" onClick={CheckAll}>Select All</button>
-            {/* <button id="clear-select" onClick={UncheckAll}>Deselect All</button> */}
+          <section className="selecting-btn-cont"> 
+          {
+            isAllChecked ? <button id="clear-select" onClick={()=>{
+
+              UncheckAll()
+              setIsAllChecked(false)
+
+            }}>Deselect All</button> :
+
+            <button id="select-all" onClick={()=> {
+                    CheckAll()
+                    setIsAllChecked(true)
+             } }
+           >Select All</button>
+          }
+
              <button className="execution-btn" id="deleting-btn" onClick={props.action}>{props.labelTitle}</button>
           </section>
         
