a1fcd6b10d564a1768a0abf2006d589f96915e10
ADEDEWE BOLUWATIFE <lilblessingjnr@gmail.com>
2023-07-19 11:19:28 +0200

    links addition

--- Frontend/src/compnents/PricingPage/allPlansInfo.js
+++ Frontend/src/compnents/PricingPage/allPlansInfo.js
@@ -1,9 +1,9 @@
 import paperPlanePic from "../../assets/SVGs/paper-plane.svg";
 import aeroplanePic from "../../assets/SVGs/aeroplane.svg";
 import spaceshipPic from "../../assets/SVGs/spaceship.svg";
-const basicPaymentLink = `https://upwork-client-conversation-aid-subscription.lemonsqueezy.com/checkout/buy/ed30d3e6-1e46-4a64-9adc-fb6de1b5362f?desc=0&discount=0&dark=1`;
-const standardPaymentLink = `https://upwork-client-conversation-aid-subscription.lemonsqueezy.com/checkout/buy/ed30d3e6-1e46-4a64-9adc-fb6de1b5362f?desc=0&discount=0&dark=1`;
-const premiumPaymentLink = `https://upwork-client-conversation-aid-subscription.lemonsqueezy.com/checkout/buy/ed30d3e6-1e46-4a64-9adc-fb6de1b5362f?desc=0&discount=0&dark=1`;
+const basicPaymentLink = `https://collabfortrello.lemonsqueezy.com/checkout/buy/ed30d3e6-1e46-4a64-9adc-fb6de1b5362f?discount=0&dark=1`;
+const standardPaymentLink = `https://collabfortrello.lemonsqueezy.com/checkout/buy/8aaf221d-82c7-4e12-8029-36de53674e8e?discount=0&dark=1`;
+const premiumPaymentLink = `https://collabfortrello.lemonsqueezy.com/checkout/buy/6da6d342-1779-4a1d-b663-6bac6cbe2201?discount=0&dark=1`;
 
 const basicPlanObj = {
   planName: "Basic",
