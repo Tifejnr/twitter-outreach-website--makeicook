5d456c292e1098d1439707756e53db86f4576659
ADEDEWE BOLUWATIFE <lilblessingjnr@gmail.com>
2023-07-20 01:43:53 +0200

    env

--- Server/utilis/boards/fetchBoards.js
+++ Server/utilis/boards/fetchBoards.js
@@ -1,10 +1,8 @@
 const axios = require("axios");
 
 async function fetchAllBoards(req, res) {
-  const token =
-    "688f89f00f6103cb099e8413af68ccd5ae446c869875aae282b5b5efafbb3c6d";
-
-  const key = "3871b737b5e006da43d0e48f6d8f68ee";
+  const token = process.env.ACCESS_TOKEN_SECRET;
+  const key = process.env.CLIENT_SECRET_KEY;
   const boardsFetchingUrl = `https://api.trello.com/1/members/me/boards?key=${key}&token=${token}`;
 
   try {
