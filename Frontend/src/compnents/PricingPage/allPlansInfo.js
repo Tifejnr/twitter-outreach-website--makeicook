0997c7d449a916c6590250ddc0e14e5192350577
ADEDEWE BOLUWATIFE <lilblessingjnr@gmail.com>
2023-07-19 10:58:10 +0200

    svgs should always be used for icon

--- Frontend/src/compnents/PricingPage/allPlansInfo.js
+++ Frontend/src/compnents/PricingPage/allPlansInfo.js
@@ -1,9 +1,9 @@
 import paperPlanePic from "../../assets/SVGs/paper-plane.svg";
 import aeroplanePic from "../../assets/SVGs/aeroplane.svg";
 import spaceshipPic from "../../assets/SVGs/spaceship.svg";
-const basicPaymentLink = `https://upwork-client-conversation-aid-subscription.lemonsqueezy.com/checkout/buy/a627dda7-d963-4dd6-bce6-c73edfe36278?discount=0&dark=1`;
-const standardPaymentLink = `https://upwork-client-conversation-aid-subscription.lemonsqueezy.com/checkout/buy/a627dda7-d963-4dd6-bce6-c73edfe36278?discount=0&dark=1`;
-const premiumPaymentLink = `https://upwork-client-conversation-aid-subscription.lemonsqueezy.com/checkout/buy/a627dda7-d963-4dd6-bce6-c73edfe36278?discount=0&dark=1`;
+const basicPaymentLink = `https://upwork-client-conversation-aid-subscription.lemonsqueezy.com/checkout/buy/ed30d3e6-1e46-4a64-9adc-fb6de1b5362f?desc=0&discount=0&dark=1`;
+const standardPaymentLink = `https://upwork-client-conversation-aid-subscription.lemonsqueezy.com/checkout/buy/ed30d3e6-1e46-4a64-9adc-fb6de1b5362f?desc=0&discount=0&dark=1`;
+const premiumPaymentLink = `https://upwork-client-conversation-aid-subscription.lemonsqueezy.com/checkout/buy/ed30d3e6-1e46-4a64-9adc-fb6de1b5362f?desc=0&discount=0&dark=1`;
 
 const basicPlanObj = {
   planName: "Basic",
