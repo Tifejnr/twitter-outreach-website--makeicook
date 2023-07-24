377ab4034a9acaef985f042b12c8fd85ae9b5b8d
ADEDEWE BOLUWATIFE <lilblessingjnr@gmail.com>
2023-07-24 00:25:38 -0400

    web url vcnaged

---
+++ Frontend/src/JS functions/dist/websiteUrl.dev.js
@@ -0,0 +1,9 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.websiteUrl = void 0;
+// export const websiteUrl = "http://localhost:3000";
+var websiteUrl = "https://www.collabfortrello.com";
+exports.websiteUrl = websiteUrl;
