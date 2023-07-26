72c0b17704fc6d086d5ea3e9f77a10cbbaf7ac17
ADEDEWE BOLUWATIFE <lilblessingjnr@gmail.com>
2023-07-26 03:58:12 -0500

    async to catch webhiik error

--- Server/routes/Payments/dist/webhooks.dev.js
+++ Server/routes/Payments/dist/webhooks.dev.js
@@ -16,35 +16,39 @@ var keysObjects = getKeys();
 var secret = keysObjects.webHookSecret; // Endpoint to handle incoming webhook events
 
 router.post("/", function (req, res) {
-  console.log(req.body);
-  var signature = Buffer.from(req.get("X-Signature") || "", "utf8");
-  console.log(signature); // Verify the signature
+  console.log(req.rawBody);
 
-  var hmac = crypto.createHmac("sha256", secret);
-  var digest = Buffer.from(hmac.update(req.rawBody).digest("hex"), "utf8");
+  try {
+    var signature = Buffer.from(req.get("X-Signature") || "", "utf8"); // Verify the signature
 
-  if (!crypto.timingSafeEqual(digest, signature)) {
-    console.log("invalid signature ma g"); // Invalid signature
+    var hmac = crypto.createHmac("sha256", secret);
+    var digest = Buffer.from(hmac.update(req.rawBody).digest("hex"), "utf8");
 
-    return res.status(403).json({
-      error: "Invalid signature."
-    });
-  } // Signature is valid, process the webhook event
+    if (!crypto.timingSafeEqual(digest, signature)) {
+      console.log("invalid signature ma g"); // Invalid signature
+
+      return res.status(403).json({
+        error: "Invalid signature."
+      });
+    } // Signature is valid, process the webhook event
 
 
-  var _req$body = req.body,
-      event = _req$body.event,
-      data = _req$body.data;
+    var _req$body = req.body,
+        event = _req$body.event,
+        data = _req$body.data;
 
-  if (event === "order_created") {
-    // Handle the successful order payment event
-    // You can perform any actions you want here, such as updating your database, sending notifications, etc.
-    console.log("Received successful order payment event:", data); // Respond with a 200 status to acknowledge receipt of the webhook
+    if (event === "order_created") {
+      // Handle the successful order payment event
+      // You can perform any actions you want here, such as updating your database, sending notifications, etc.
+      console.log("Received successful order payment event:", data); // Respond with a 200 status to acknowledge receipt of the webhook
 
-    return res.sendStatus(200);
-  } else {
-    // For other events, respond with a 204 status to indicate that the event is not handled by this endpoint
-    return res.sendStatus(204);
+      return res.sendStatus(200);
+    } else {
+      // For other events, respond with a 204 status to indicate that the event is not handled by this endpoint
+      return res.sendStatus(204);
+    }
+  } catch (error) {
+    console.log(error);
   }
 });
 module.exports = router;
--- Server/routes/Payments/webhooks.js
+++ Server/routes/Payments/webhooks.js
@@ -10,31 +10,34 @@ const secret = keysObjects.webHookSecret;
 
 // Endpoint to handle incoming webhook events
 router.post("/", (req, res) => {
-  console.log(req.body);
-  const signature = Buffer.from(req.get("X-Signature") || "", "utf8");
+  console.log(req.rawBody);
 
-  console.log(signature);
+  try {
+    const signature = Buffer.from(req.get("X-Signature") || "", "utf8");
 
-  // Verify the signature
-  const hmac = crypto.createHmac("sha256", secret);
-  const digest = Buffer.from(hmac.update(req.rawBody).digest("hex"), "utf8");
+    // Verify the signature
+    const hmac = crypto.createHmac("sha256", secret);
+    const digest = Buffer.from(hmac.update(req.rawBody).digest("hex"), "utf8");
 
-  if (!crypto.timingSafeEqual(digest, signature)) {
-    console.log("invalid signature ma g");
-    // Invalid signature
-    return res.status(403).json({ error: "Invalid signature." });
-  }
-  // Signature is valid, process the webhook event
-  const { event, data } = req.body;
-  if (event === "order_created") {
-    // Handle the successful order payment event
-    // You can perform any actions you want here, such as updating your database, sending notifications, etc.
-    console.log("Received successful order payment event:", data);
-    // Respond with a 200 status to acknowledge receipt of the webhook
-    return res.sendStatus(200);
-  } else {
-    // For other events, respond with a 204 status to indicate that the event is not handled by this endpoint
-    return res.sendStatus(204);
+    if (!crypto.timingSafeEqual(digest, signature)) {
+      console.log("invalid signature ma g");
+      // Invalid signature
+      return res.status(403).json({ error: "Invalid signature." });
+    }
+    // Signature is valid, process the webhook event
+    const { event, data } = req.body;
+    if (event === "order_created") {
+      // Handle the successful order payment event
+      // You can perform any actions you want here, such as updating your database, sending notifications, etc.
+      console.log("Received successful order payment event:", data);
+      // Respond with a 200 status to acknowledge receipt of the webhook
+      return res.sendStatus(200);
+    } else {
+      // For other events, respond with a 204 status to indicate that the event is not handled by this endpoint
+      return res.sendStatus(204);
+    }
+  } catch (error) {
+    console.log(error);
   }
 });
 
