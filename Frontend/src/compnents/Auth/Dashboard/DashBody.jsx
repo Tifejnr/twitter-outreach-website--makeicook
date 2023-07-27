c6ec337d3760adc18f2f1aba2ebf931a0851ba8d
ADEDEWE BOLUWATIFE <lilblessingjnr@gmail.com>
2023-07-26 20:47:26 -0500

    back to dahsboard design

--- Frontend/src/compnents/Auth/Dashboard/DashBody.jsx
+++ Frontend/src/compnents/Auth/Dashboard/DashBody.jsx
@@ -1,8 +1,8 @@
-import React, {useEffect, useState} from 'react'
-import { Link } from 'react-router-dom';
-import accountIcon from "../../../assets/SVGs/account.svg"
-import logoutIcon from "../../../assets/SVGs/logout.svg"
-import trial from '../../../JS functions/Auth/trail';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import accountIcon from "../../../assets/SVGs/account.svg";
+import logoutIcon from "../../../assets/SVGs/logout.svg";
+import trial from "../../../JS functions/Auth/trail";
 
 export default function DashBody() {
   const [username, setUsername] = useState("username");
@@ -10,62 +10,72 @@ export default function DashBody() {
   const [credits, setCredits] = useState(5);
   const [creditsExpiration, setCreditsExpiration] = useState("Never");
 
-
-
   return (
- <>
+    <>
+      <main>
+        <section className="profile">
+          <section className="profileBlock">
+            <picture className="userAccountIconContainer" title="My Profile">
+              <img
+                src={accountIcon}
+                alt="account icon"
+                className="userAccountIcon"
+              />
+            </picture>
 
- <main>
-      <section className="profile">
-        <section className="profileBlock">
-          <picture className="userAccountIconContainer" title="My Profile"
-            ><img
-              src={accountIcon}
-              alt="account icon"
-              className="userAccountIcon" />
-          </picture>
+            <section className="profileDetails">
+              <article className="name">
+                <h2>{username}</h2>
+              </article>
+              <article className="email">
+                <p>{email}</p>
+              </article>
+              <table>
+                <tbody>
 
-          <section className="profileDetails">
-            <article className="name"><h2>{username}</h2></article>
-            <article className="email"><p>{email}</p></article>
-            <table>
-              <tbody>
-                <tr>
-                  <td>Credits</td>
-                  <td className="value upLink">{credits}</td>
-                </tr>
-                <tr>
-                  <td>Credits Expiration</td>
-                  <td className="value jobsPiadFor">{creditsExpiration}</td>
-                </tr>
-                <tr>
-                  <td className="jobRelatedKeyword">Jobs Completed</td>
-                  <td className="value">kaka</td>
-                </tr>
-                <tr>
-                  <td className="jobRelatedKeyword">To Be Completed</td>
-                  <td className="value">"jaj"</td>
-                </tr>
-              </tbody>
-            </table>
+                  <tr>
+                    <td>Credits</td>
+                    <td className="value upLink">{credits}</td>
+                  </tr>
+                  <tr>
+                    <td>Credits Expiration</td>
+                    <td className="value jobsPiadFor">{creditsExpiration}</td>
+                  </tr>
+
+                </tbody>               
+              </table>
+
+              <button>Buy Credits</button>
+
+              <table>
+                 <tbody>
+                  <tr>
+                    <td className="jobRelatedKeyword">Jobs Completed</td>
+                    <td className="value">kaka</td>
+                  </tr>
+                  <tr>
+                    <td className="jobRelatedKeyword">To Be Completed</td>
+                    <td className="value">"jaj"</td>
+                  </tr>
+                </tbody>
+              </table>
+            </section>
           </section>
         </section>
-      </section>
-    </main>
+      </main>
 
-   <section className="logoutContainer" onClick={async (e)=> {
-    e.preventDefault(
-    await  trial()
-    )
-   }}>
-      <Link htmlFor="/logout" className="logoutLink">
-        <picture className="logoutIcon" title="Logout">
-          <img src={logoutIcon} alt="logout icon"
-        /></picture>
-        <h3>Logout</h3>
-      </Link>
-   </section>
- 
- </>
-  )
+      <section
+        className="logoutContainer"
+        onClick={async (e) => {
+          e.preventDefault(await trial());
+        }}>
+        <Link htmlFor="/logout" className="logoutLink">
+          <picture className="logoutIcon" title="Logout">
+            <img src={logoutIcon} alt="logout icon" />
+          </picture>
+          <h3>Logout</h3>
+        </Link>
+      </section>
+    </>
+  );
 }
--- Frontend/src/compnents/Auth/Dashboard/Dashboard.jsx
+++ Frontend/src/compnents/Auth/Dashboard/Dashboard.jsx
@@ -5,70 +5,7 @@ import NavLogo from '../../Main-nav-bar/NavLogo'
 import AuthFooter from '../AuthFooter'
 
 export default function Dashboard() {
-  const buttonClick = ()=> {
- async function postData() {
-    const url = "https://www.collabfortrello.com/api/sign-in";
-    const data = {
-      imado: "JGuners",
-      lakaka: "johndoe@example.com",
-    };
 
-    try {
-      const response = await fetch(url, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      });
-
-      console.log(response)
-
-      if (!response.ok) {
-        throw new Error("Request failed with status: " + response.status);
-      }
-
-      const responseData = await response.json();
-      console.log(responseData);
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  postData();
-  }
-
-
-  const postData2= ()=> {
-    async function postData() {
-    const url = `https://workforreputation.com/api/auth`;
-    const data = {
-      knamelmn: "loiama Doe",
-      email: "johndoe@example.com",
-    };
-
-    try {
-      const response = await fetch(url, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      });
-
-      if (!response.ok) {
-        throw new Error("Request failed with status: " + response.status);
-      }
-
-      const responseData = await response.json();
-      console.log(responseData);
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  postData();
-  }
   return (
     <>
   <nav className='nav'>
@@ -88,9 +25,6 @@ export default function Dashboard() {
   </nav> 
 
   <DashBody />
-
-  <button onClick={buttonClick}>button click me</button>
-  <button onClick={postData2}>post to wfr</button>
 
   <AuthFooter/>
     </>
