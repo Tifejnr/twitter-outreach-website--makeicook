95d710f0ea993ac5ffbc107efe643830dc467876
ADEDEWE BOLUWATIFE <lilblessingjnr@gmail.com>
2023-07-27 10:57:34 +0200

    refactoring dahsboard page

--- Frontend/src/compnents/Auth/Dashboard/DashBody.jsx
+++ Frontend/src/compnents/Auth/Dashboard/DashBody.jsx
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import accountIcon from "../../../assets/SVGs/account.svg";
 import logoutIcon from "../../../assets/SVGs/logout.svg";
 import trial from "../../../JS functions/Auth/trail";
+import ChangePassword from "./Sections/ChangePassword";
+import ProfileDetails from "./Sections/ProfileDetails";
 
 export default function DashBody() {
   const [username, setUsername] = useState("username");
@@ -13,96 +15,12 @@ export default function DashBody() {
   return (
     <>
       <main className="dashboard-container">
-        <section className="profile">
-          <section className="profileBlock">
-            <picture className="userAccountIconContainer" title="My Profile">
-              <img
-                src={accountIcon}
-                alt="account icon"
-                className="userAccountIcon"
-              />
-            </picture>
-
-            <section className="profileDetails">
-              <article className="name">
-                <h2>{username}</h2>
-              </article>
-              <article className="email">
-                <p>{email}</p>
-              </article>
-              <table>
-                <tbody>
-                  <tr>
-                    <td>Credits</td>
-                    <td className="value">{credits}</td>
-                  </tr>
-                  <tr>
-                    <td>Expiration</td>
-                    <td className="value">{creditsExpiration}</td>
-                  </tr>
-
-                  <tr>
-                    <td>Plan</td>
-                    <td className="value">{"Premium"}</td>
-                  </tr>
-                  <tr>
-                    <td>Expiration</td>
-                    <td className="value">{creditsExpiration}</td>
-                  </tr>
-                </tbody>
-              </table>
-
-              <button>Buy Credits</button>
-            </section>
-          </section>
-        </section>
-
-
-             <section className="change-password-section">
-        <h2>CHANGE ACCOUNT PASSWORD</h2>
-        <fieldset className="input-wrapper">
-          <label htmlFor="emailId">
-            <p>Old Password:</p>
-          </label>
-          <input
-            type="email"
-            placeholder="Enter your email"
-            id="emailId"
-            value={"email"}
-            //  onChange={(e)=> setEmail(e.target.value)}
-          />
-          <p className="error"></p>
-        </fieldset>
-        <fieldset className="input-wrapper">
-          <label htmlFor="passwordId">
-            <p>New Password:</p>
-          </label>
-          <input
-            type="password"
-            placeholder="Enter your password"
-            id="passwordId"
-            value={"password"}
-            //  onChange={(e)=> setPassword(e.target.value)}
-          />
-          <p className="error" id="regErrorDisplay"></p>
-        </fieldset>
-      </section>
-
+       <ProfileDetails/>
+        <ChangePassword />
       </main>
 
  
-      <section
-        className="logoutContainer"
-        onClick={async (e) => {
-          e.preventDefault(await trial());
-        }}>
-        <Link htmlFor="/logout" className="logoutLink">
-          <picture className="logoutIcon" title="Logout">
-            <img src={logoutIcon} alt="logout icon" />
-          </picture>
-          <h3>Logout</h3>
-        </Link>
-      </section>
+   
     </>
   );
 }
--- Frontend/src/compnents/Auth/Dashboard/Dashboard.jsx
+++ Frontend/src/compnents/Auth/Dashboard/Dashboard.jsx
@@ -3,24 +3,24 @@ import goBackIcon from "../../../assets/SVGs/goBack.svg"
 import DashBody from './DashBody'
 import NavLogo from '../../Main-nav-bar/NavLogo'
 import AuthFooter from '../AuthFooter'
+import LandingPageToggle from '../../Main-nav-bar/LandingPageToggle'
+import NavItemsDashBoard from './NavItemsDashBoard'
 
-export default function Dashboard() {
+export default function Dashboard() { 
 
   return (
     <>
-  <nav className='nav'>
-       <label for="nav__checkbox" class="nav__toggle">
-          <picture className="cartIcon" id="goBackIcon" title="Go Back">
-            <img src={goBackIcon} alt="go back icon"
-          /></picture>
-        </label>
+    <nav className='nav'>
+       
+       <LandingPageToggle/>
 
       <ul className="nav__menu">
 
         <li>
           <NavLogo />
         </li>
-
+       
+      <NavItemsDashBoard/>  
       </ul>
   </nav> 
 
