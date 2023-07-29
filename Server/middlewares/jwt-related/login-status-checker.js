5fd1494d772230b80215a2b1a343f093abd09ac8
ADEDEWE BOLUWATIFE <lilblessingjnr@gmail.com>
2023-07-29 20:31:07 +0200

    push to serevr

--- Server/middlewares/jwt-related/login-status-checker.js
+++ Server/middlewares/jwt-related/login-status-checker.js
@@ -4,8 +4,8 @@ const keysObject = getKeys();
 const JWT_PRIVATE_KEY = keysObject.JWT_PRIVATE_KEY;
 
 module.exports = function (req, res, next) {
-  // const token = req.cookies.cftAuth;
-  const token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGFkODBiNjMxODI1Njc2YTNmY2VjNzciLCJpc1BhaWQiOmZhbHNlLCJpYXQiOjE2OTA2MDYyNDQsImV4cCI6MTY5MzE5ODI0NH0.eWQOJxMQNsu8xKIxcm8YcJr1dUNakCfO3pg5buZuqgk`;
+  const token = req.cookies.cftAuth;
+  // const token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGFkODBiNjMxODI1Njc2YTNmY2VjNzciLCJpc1BhaWQiOmZhbHNlLCJpYXQiOjE2OTA2MDYyNDQsImV4cCI6MTY5MzE5ODI0NH0.eWQOJxMQNsu8xKIxcm8YcJr1dUNakCfO3pg5buZuqgk`;
 
   if (!token) return res.status(401).json({ invalidJWT: true });
 
