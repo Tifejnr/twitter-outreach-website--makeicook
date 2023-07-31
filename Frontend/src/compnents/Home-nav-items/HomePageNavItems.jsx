57accc35b2455ca7961597cecef1b640fc30a19a
ADEDEWE BOLUWATIFE <lilblessingjnr@gmail.com>
2023-07-31 04:25:54 +0200

    now to building foote section

--- Frontend/src/compnents/Home-nav-items/HomePageNavItems.jsx
+++ Frontend/src/compnents/Home-nav-items/HomePageNavItems.jsx
@@ -5,12 +5,9 @@ export default function HomePageNavItems(props) {
 
  return (
       <>
-        <CustomLink to={"/pricing"} className="nav-list" >
-          <h3 className="pricing">Pricing</h3>
-        </CustomLink>
 
         <CustomLink className="nav-list">
-            <h3 className="reviews">Reviews</h3>
+            <h3 className="reviews">Credits Usage</h3>
         </CustomLink>
 
        <li  className="sign-up-cont" id="credits-no-large-screen">
@@ -18,8 +15,8 @@ export default function HomePageNavItems(props) {
          </h4>
       </li>
 
-        <CustomLink to="/register" >
-        <h3 className='sign-upa'>Contact us</h3> 
+        <CustomLink to={"/pricing"} className="nav-list" >
+          <h3 className="pricing">Buy Credits</h3>
         </CustomLink>
 </>
 )
