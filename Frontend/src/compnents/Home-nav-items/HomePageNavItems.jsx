a9e17d01fdb01af6d78d028397c1059a2a313215
ADEDEWE BOLUWATIFE <lilblessingjnr@gmail.com>
2023-08-01 02:28:04 +0200

    drop down menu fo rtools

--- Frontend/src/compnents/Home-nav-items/HomePageNavItems.jsx
+++ Frontend/src/compnents/Home-nav-items/HomePageNavItems.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import CustomLink from '../CustomLink'
+import { Link } from 'react-router-dom'
 
 export default function HomePageNavItems(props) {
 
  return (
       <>
-
+        <li className='tools-dropdown'>
+              <Link htmlFor="#">Tools</Link>
+              <ul className="dropdown-content">
+                <li><Link htmlFor="#">Add to boards</Link></li>
+                <li><Link htmlFor="#">Remove from boars</Link></li>
+                <li><Link htmlFor="#">Add to workspaces</Link></li>
+                <li><Link htmlFor="#">Remove from workspaces</Link></li>
+              </ul>
+       </li>
         <CustomLink className="nav-list">
-            <h3 className="reviews">Credits Usage</h3>
+            <h3 className="creditsUsage">Credits Usage</h3>
         </CustomLink>
 
        <li  className="sign-up-cont" id="credits-no-large-screen">
