e0963595552f733dc18638801577efc9f2cb4c9e
ADEDEWE BOLUWATIFE <lilblessingjnr@gmail.com>
2023-08-15 18:42:39 -0400

    ui changies to accomade both deleting by username and fullanem within an input

--- Frontend/src/compnents/Pages/DeleteMemberBoards.jsx
+++ Frontend/src/compnents/Pages/DeleteMemberBoards.jsx
@@ -16,15 +16,11 @@ import getWorkspacesName from "./getWorkspacesName";
 import getAllBoardsId from "./Validations/getBoardIdOnly/getAllBoardsId";
 
 
-//username means  input params
-const usernameMeansInputLabel = "Members' Usernames:";
-const usernameMeansInputPlaceholderText =
-  "Input usernames of members to be deleted, each separated with a comma.";
 
-//fullname means input params
-const fullnameMeansInputLabel = "Members' Full names:";
+//name means input params
+const fullnameMeansInputLabel = "Members' Usernames or Full names:";
 const fullnameMeansInputPlaceholderText =
-  "Input fullnames of members to be deleted, each separated with a comma.";
+  "Input usernames or fullnames of members to be deleted, each separated with a comma.";
 
 const searchPlaceholderTitle = "Search Boards ...";
 const selectInstructionText = "Select Boards to Delete Members from";
@@ -63,8 +59,7 @@ export default function DeleteMemberBoards() {
   const  setExecutionErrorBtn = useStore((state) => state.setExecutionErrorBtn);
   const  pushWorkspaceObjDetails = useStore((state) => state.pushWorkspaceObjDetails);
   const  workspaceObjDetails = useStore((state) => state.workspaceObjDetails);
-  const  meansOfExceution = useStore((state) => state.meansOfExceution);
-  const setMeansOfExceution = useStore((state) => state.setMeansOfExceution);
+
 
 
   changeTabTitle(addToBoardsTabTitle)
@@ -78,7 +73,6 @@ export default function DeleteMemberBoards() {
     checkboxesArray,
     boardDetailsObj,
     boardIdsObj,
-    meansOfExceution,
     executionBtnClicked,
     action,
     continuousAction,
@@ -121,7 +115,7 @@ export default function DeleteMemberBoards() {
    setTextAreaError("")
    setExecutionErrorBtn("")
 
-   if (meansOfExceution==usernameMeans) {   
+   if (response.isusernameInput) {   
      if (response.errorNameAddingObjArray )  {
   const usernamesAtAdded = response.errorNameAddingObjArray.map((username) => {
     return `@${username}`
@@ -163,7 +157,7 @@ export default function DeleteMemberBoards() {
     }
     
 
-   if (meansOfExceution==fullNameMeans) {  
+  else {  
   
    if (response.stop) {
     const stoppedMessage = "Action Stopped"
@@ -256,11 +250,7 @@ export default function DeleteMemberBoards() {
   }, []);
 
   useEffect(()=> {
-     // auto pick means picked previously for users
-    const meansChosenDeleteFromBoards= localStorage.getItem('meansChosenDeleteFromBoards');
-    if (meansChosenDeleteFromBoards) 
-   return setMeansOfExceution(meansChosenDeleteFromBoards), setSelectLabel(meansChosenDeleteFromBoards);
-   return setSelectLabel(defaultMeansMessage), setMeansOfExceution(defaultMeansMessage);
+   
     
   })
 
@@ -278,25 +268,15 @@ export default function DeleteMemberBoards() {
       <section
         className="main-section-cont"
         id="mainContentCont">
-        <h1 id="toolInstruction">{pageTitle} {meansOfExceution==defaultMeansMessage ? unknowMeansYet: meansOfExceution}</h1>
-        <SelectMeans actionToBePerformed={action} selectLabel={selectLabel}/>
+        <h1 id="toolInstruction">{pageTitle} Username or Full name</h1>
 
-      { meansOfExceution == defaultMeansMessage ? "" :  <section className="inner-main-cont" id="innerMainContentCont">
-        {
-           meansOfExceution == usernameMeans ? 
+      <section className="inner-main-cont" id="innerMainContentCont">
           <Input
-            inputLabel={usernameMeansInputLabel}
-            inputPlaceholderText={usernameMeansInputPlaceholderText}
-            textAreaError={textAreaError}
-          /> :   
-          
-          meansOfExceution == fullNameMeans ?  
-             <Input
             inputLabel={fullnameMeansInputLabel}
             inputPlaceholderText={fullnameMeansInputPlaceholderText}
             textAreaError={textAreaError}
-          /> :    ""
-        } 
+          />   
+
           <SelectAll
             labelTitle={labelTitle}
             verifying = "Verifying Inputs..."
@@ -322,9 +302,8 @@ export default function DeleteMemberBoards() {
             }
           </section>
         </section>
-}
       </section>
-         </>
+    </>
          }
     </>
   );
