1c40284c7f866ad5a7e0862c4beff04af5ebdc53
ADEDEWE BOLUWATIFE <lilblessingjnr@gmail.com>
2023-08-21 15:52:22 +0200

    push to prod again

--- Frontend/src/compnents/Pages/AddMember.jsx
+++ Frontend/src/compnents/Pages/AddMember.jsx
@@ -286,7 +286,7 @@ export default function AddMember() {
             </section>
         } 
     
-        <section className="boardsListSection">
+        <section className="boardsListSection" id="boardsListSection">
                 <SelectAll
                   labelTitle={labelTitle}
                   verifying="Verifying Inputs..."
