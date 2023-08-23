01246ad007f71d9bc9cf38f0190d8416c13b1657
ADEDEWE BOLUWATIFE <lilblessingjnr@gmail.com>
2023-08-23 11:55:38 -0400

    commentin added to the codes using chatgpt

--- Frontend/src/compnents/Pages/BasicSectionLayout/BoardsDisplaySection.jsx
+++ Frontend/src/compnents/Pages/BasicSectionLayout/BoardsDisplaySection.jsx
@@ -1,20 +1,22 @@
 import { useState, useEffect, useRef } from "react";
 import useStore from "../../Hooks/Zustand/usersStore";
-import openBoardsDetailIcon from "../../../assets/SVGs/faq-toggle-icon.svg"
+import openBoardsDetailIcon from "../../../assets/SVGs/faq-toggle-icon.svg";
 
+// This component displays board information.
 export default function BoardsDisplaySection(props) {
-  const [isClicked, setIsClicked] = useState(false);
-  const [boardMembersObj, setBoardMembersObj] = useState([]);
-  const checkboxRef = useRef(null);
-  const checkboxesArray = useStore((state) => state.checkboxesArray);
-  const pushCheckboxesArray = useStore((state) => state.pushCheckboxesArray);
+  const [isClicked, setIsClicked] = useState(false); // State to track click events.
+  const [boardMembersObj, setBoardMembersObj] = useState([]); // State to store board members.
+  const checkboxRef = useRef(null); // Reference to the checkbox element.
+  const checkboxesArray = useStore((state) => state.checkboxesArray); // Get checkboxes from Zustand state.
+  const pushCheckboxesArray = useStore((state) => state.pushCheckboxesArray); // Get a function to update checkboxes from Zustand.
   const setCheckboxRatioNotifierDisplay = useStore(
     (state) => state.setCheckboxRatioNotifierDisplay
-  );
+  ); // Get a function to display checkbox ratio from Zustand.
 
-  const workspaceObjDetails = props.workspaceObjDetails;
-  const board = props.board
+  const workspaceObjDetails = props.workspaceObjDetails; // Workspace details.
+  const board = props.board; // Board details.
 
+  // Function to calculate and display the checkbox ratio.
   const checkboxRatioNotifier = () => {
     const totalCheckboxes = checkboxesArray.length;
     const noOfChecked = checkboxesArray.filter(
@@ -25,46 +27,48 @@ export default function BoardsDisplaySection(props) {
     setCheckboxRatioNotifierDisplay(checkboxRatioNotifierValue);
   };
 
-    //getting product details first
-    const isWorkspaceDetailsValid = workspaceObjDetails.find(
-      (workspaceDetail) => workspaceDetail.workspaceId == board.idOrganization
-    );  
+  // Check if the workspace details are valid for the board.
+  const isWorkspaceDetailsValid = workspaceObjDetails.find(
+    (workspaceDetail) => workspaceDetail.workspaceId == board.idOrganization
+  );
 
-
+  // useEffect to push the checkbox element into the checkboxes array when the component mounts.
   useEffect(() => {
     const checkboxEle = checkboxRef.current;
     pushCheckboxesArray(checkboxEle);
-
   }, []);
 
+  // Function to load and display board members when the "show members" button is clicked.
+  function loadAllBoardMembersName() {
+    setIsClicked((prevState) => !prevState);
+    const memberRawArrayDetail = props.memberRawArrayDetail;
 
-  function loadAllBoardMembersName () {
+    // Find board members for the current board.
+    const allboardMembersObj = memberRawArrayDetail.find(
+      (memberDetail) => memberDetail.boardId === board.id
+    );
+    const boardMembersObjAlone = allboardMembersObj.boardMembersDetails;
 
-  setIsClicked((prevState)=>!prevState)  
-  const memberRawArrayDetail = props.memberRawArrayDetail;
-  //find board members for each boards
-  const allboardMembersObj = memberRawArrayDetail.find((memberDetail) => memberDetail.boardId === board.id);
-  const boardMembersObjAlone = allboardMembersObj.boardMembersDetails;
-
-  if (boardMembersObjAlone) return setBoardMembersObj(boardMembersObjAlone)
-
+    if (boardMembersObjAlone) return setBoardMembersObj(boardMembersObjAlone);
   }
 
-  //setting toglig
-   const rotateOnToggle = {
-    transform: isClicked && "rotate(180deg)"
+  // Style for rotating the "show members" icon.
+  const rotateOnToggle = {
+    transform: isClicked && "rotate(180deg)",
   };
 
- const openFaqDetailsStyle= {
-     maxHeight: isClicked ?  "100%" :"0",
-        marginTop: isClicked ? '0.8rem' :"-0.4rem",
-        overflow: isClicked ?  'visible': "hidden",
- }
+  // Style for opening/closing the member list.
+  const openFaqDetailsStyle = {
+    maxHeight: isClicked ? "100%" : "0",
+    marginTop: isClicked ? "0.8rem" : "-0.4rem",
+    overflow: isClicked ? "visible" : "hidden",
+  };
 
+  // Render the component.
   return (
     <form className="boardListItem eachMemberListCont" name="main">
-      <section className='member-info-container'>
-          <input
+      <section className="member-info-container">
+        <input
           onClick={checkboxRatioNotifier}
           type="checkbox"
           name="fruit"
@@ -73,30 +77,35 @@ export default function BoardsDisplaySection(props) {
           className="inputs board-checkbox"
           id={`check${props.indexNo}`}
         />
-      <article onClick={loadAllBoardMembersName} title="Click to show members">
-
-        <p id={`labelcheck${props.indexNo}`}>        
-          {board.name} 
-           
-           <picture title="Click to show members">
-              <img style={rotateOnToggle} src={openBoardsDetailIcon} alt="show member icon" />
+        <article onClick={loadAllBoardMembersName} title="Click to show members">
+          <p id={`labelcheck${props.indexNo}`}>
+            {board.name}
+            <picture title="Click to show members">
+              <img
+                style={rotateOnToggle}
+                src={openBoardsDetailIcon}
+                alt="show member icon"
+              />
             </picture>
-        </p>
-
-        <p>{ isWorkspaceDetailsValid ? `WS - ${isWorkspaceDetailsValid.workspaceName}`: "WS - None"}</p>
-
-         <ul style={openFaqDetailsStyle}>
+          </p>
+          <p>
+            {isWorkspaceDetailsValid
+              ? `WS - ${isWorkspaceDetailsValid.workspaceName}`
+              : "WS - None"}
+          </p>
+          <ul style={openFaqDetailsStyle}>
             <h3>Members:</h3>
-
-            {boardMembersObj.length > 0 && boardMembersObj.map((memberDetail, index)=> {
-                return <li key={index}>{index+1}. {memberDetail.fullName}</li>
+            {boardMembersObj.length > 0 &&
+              boardMembersObj.map((memberDetail, index) => {
+                return (
+                  <li key={index}>
+                    {index + 1}. {memberDetail.fullName}
+                  </li>
+                );
               })}
-          </ul> 
-
-    </article>
-    </section>
+          </ul>
+        </article>
+      </section>
     </form>
   );
 }
-
-
--- Frontend/src/compnents/Pages/BasicSectionLayout/MemberInfoDisplay.jsx
+++ Frontend/src/compnents/Pages/BasicSectionLayout/MemberInfoDisplay.jsx
@@ -1,123 +1,135 @@
-import { useRef, useEffect , useState } from "react";
+import { useRef, useEffect, useState } from "react";
 import useStore from "../../Hooks/Zustand/usersStore";
-import openMemberDetailIcon from "../../../assets/SVGs/faq-toggle-icon.svg"
+import openMemberDetailIcon from "../../../assets/SVGs/faq-toggle-icon.svg";
 
-const memberNotInAnyBoardMessage = "You don't have access to some of the boards below."
-const limitOfCheckboxReached = "You can't select more than 20 members at a go."
-let allBoardMemberBelongsArray=[];
+// Define some constant messages
+const memberNotInAnyBoardMessage = "You don't have access to some of the boards below.";
+const limitOfCheckboxReached = "You can't select more than 20 members at a go.";
+let allBoardMemberBelongsArray = []; // Initialize an array to store board names.
 
+// This is the main component function.
 export default function MemberInfoDisplay(props) {
-  const checkboxRef = useRef(null);
-  const [isClicked, setIsClicked] = useState(false);
-  const memberCheckboxesArray = useStore((state) => state.memberCheckboxesArray);
-  const pushMemberCheckboxesArray = useStore((state) => state.pushMemberCheckboxesArray);
-  const setExecutionErrorBtn = useStore((state) => state.setExecutionErrorBtn);
+  const checkboxRef = useRef(null); // Reference to the checkbox element.
+  const [isClicked, setIsClicked] = useState(false); // State for tracking clicks.
+  const memberCheckboxesArray = useStore((state) => state.memberCheckboxesArray); // Get member checkboxes from Zustand state.
+  const pushMemberCheckboxesArray = useStore((state) => state.pushMemberCheckboxesArray); // Get a function to update the checkboxes array from Zustand.
+  const setExecutionErrorBtn = useStore((state) => state.setExecutionErrorBtn); // Get a function to set an error message from Zustand.
 
+  // useEffect is used for side effects in React. This effect runs once when the component mounts.
   useEffect(() => {
-
+    // If the member is a board admin, push the checkbox reference to the checkboxes array.
     if (isBoardMemberAdmin) {
-    const checkboxEle = checkboxRef.current;
-    pushMemberCheckboxesArray(checkboxEle);
+      const checkboxEle = checkboxRef.current;
+      pushMemberCheckboxesArray(checkboxEle);
     }
   }, []);
 
+  // Extract member details and board information from props.
   const memberDetailObj = props.memberDetailObj;
   const memberId = memberDetailObj.id;
   const boardIdsMapMemberId = props.boardIdsMapMemberId;
   const boardsCollection = props.boardsCollection;
 
-    //getting all board names that each memeber belongs to
-function getBoardsForMember(memberId, boardIdsMapMemberId) {
-  if (boardIdsMapMemberId[memberId]) {
-    return boardIdsMapMemberId[memberId];
+  // Function to get the boards a member belongs to.
+  function getBoardsForMember(memberId, boardIdsMapMemberId) {
+    if (boardIdsMapMemberId[memberId]) {
+      return boardIdsMapMemberId[memberId];
+    } else {
+      return []; // Return an empty array if the memberId is not found in boardIdsMapMemberId.
+    }
+  }
+
+  // Initialize some variables.
+  const memberBoardsArray = getBoardsForMember(memberId, boardIdsMapMemberId);
+  let isBoardMemberAdmin = true; // Assuming the member is initially considered an admin.
+  let memberIsNotYours = memberNotInAnyBoardMessage;
+  let boardName;
+
+  // Check if the member belongs to any boards.
+  if (memberBoardsArray.length > 0) {
+    // Map the member's board IDs to board names.
+    allBoardMemberBelongsArray = memberBoardsArray.map((boardId) => {
+      // Check if the board exists in the collection.
+      const isMemberPartOfBoard = boardsCollection.find(
+        (boardDetail) => boardDetail.id === boardId
+      );
+
+      if (!isMemberPartOfBoard) return;
+
+      boardName = isMemberPartOfBoard.name;
+      return boardName;
+    });
   } else {
-    return []; // Return an empty array if the memberId is not found in boardIdsMapMemberId
-  }
-}
-
-const memberBoardsArray = getBoardsForMember(memberId, boardIdsMapMemberId);
- let isBoardMemberAdmin = true; let memberIsNotYours = memberNotInAnyBoardMessage,  boardName;
-
-if (memberBoardsArray.length > 0) {
-  allBoardMemberBelongsArray = memberBoardsArray.map((boardId) => {
-    const isMemberPartOfBoard = boardsCollection.find(
-      (boardDetail) => boardDetail.id === boardId
-    );
-
-    if (!isMemberPartOfBoard)  return  
-
-     boardName = isMemberPartOfBoard.name;
-    return boardName;
-  });
-} else {
-
-  return  isBoardMemberAdmin = false
-}
-   //setting toggling when clicked
-  const handleToggle= ()=> {
-     setIsClicked((prevState)=>!prevState)
+    // If the member doesn't belong to any boards, update the admin status.
+    isBoardMemberAdmin = false;
   }
 
-   const rotateOnToggle = {
-    transform: isClicked && "rotate(180deg)"
+  // Function to toggle the open/close state.
+  const handleToggle = () => {
+    setIsClicked((prevState) => !prevState);
   };
 
- const openBoardsListStyle= {
-        maxHeight: isClicked ?  "100%" : "0",
-        marginTop: isClicked ? '0.8rem' : "-0.4rem",
-        overflow: isClicked ?  'visible' : "hidden",
-  } 
+  // Style for rotating the toggle icon.
+  const rotateOnToggle = {
+    transform: isClicked && "rotate(180deg)",
+  };
 
- //limit no of checkboxes of member to 20  
-function limitOnCheckableMembersNo() {
-  const noOfChecked = memberCheckboxesArray.filter(
-    (checkbox) => checkbox.checked
-  ).length;
+  // Style for opening/closing the boards list.
+  const openBoardsListStyle = {
+    maxHeight: isClicked ? "100%" : "0",
+    marginTop: isClicked ? "0.8rem" : "-0.4rem",
+    overflow: isClicked ? "visible" : "hidden",
+  };
 
-  const checkboxEle = checkboxRef.current;
+  // Function to limit the number of checked checkboxes.
+  function limitOnCheckableMembersNo() {
+    const noOfChecked = memberCheckboxesArray.filter(
+      (checkbox) => checkbox.checked
+    ).length;
 
-  if (noOfChecked > 20) {
-    // If the number of checked checkboxes is greater than 2, set an error state and uncheck the current checkbox.
-    setExecutionErrorBtn(limitOfCheckboxReached); // Assuming "limitOfCheckboxReached" is a string error message.
-    checkboxEle.checked = false;
-  } 
-}
+    const checkboxEle = checkboxRef.current;
 
-return (
-  isBoardMemberAdmin && (
-    <form className="member-list-form eachMemberListCont">
-      <section className='member-info-container'>
-        <input
-        onClick={limitOnCheckableMembersNo}
-          type="checkbox"
-          name="select-member"
-          title="Check to select member"
-          ref={checkboxRef}
-          className="inputs board-checkbox"
-          id={`checkMembers${props.indexNo}`}
-        />
-        <article onClick={handleToggle} title="Click to see boards member belongs to">
-          <p id={`fullname${props.indexNo}`}>{memberDetailObj.fullName}
-           
-           <picture title="Show boards member belongs to">
-              <img style={rotateOnToggle} src={openMemberDetailIcon} alt="show member boards icon" />
-            </picture>
-          </p>
-          <p id={`username${props.indexNo}`}>@{memberDetailObj.username}</p>
+    if (noOfChecked > 20) {
+      // If the number of checked checkboxes is greater than 20, set an error state and uncheck the current checkbox.
+      setExecutionErrorBtn(limitOfCheckboxReached);
+      checkboxEle.checked = false;
+    }
+  }
 
-          <ul style={openBoardsListStyle}>
-            <h3>Boards member belongs to:</h3>
-      
-            {allBoardMemberBelongsArray.length > 0 && (
-              Array.from(allBoardMemberBelongsArray).map((boardName, index) => {
-                return <li key={index}>{index + 1}. {boardName}</li>
-              })
-            )} 
-          </ul>
-        </article>
-      </section>
-    </form>
-  )
-)
+  // Render the component.
+  return (
+    isBoardMemberAdmin && ( // Render the following only if the member is an admin.
+      <form className="member-list-form eachMemberListCont">
+        <section className="member-info-container">
+          <input
+            onClick={limitOnCheckableMembersNo}
+            type="checkbox"
+            name="select-member"
+            title="Check to select member"
+            ref={checkboxRef}
+            className="inputs board-checkbox"
+            id={`checkMembers${props.indexNo}`}
+          />
+          <article onClick={handleToggle} title="Click to see boards member belongs to">
+            <p id={`fullname${props.indexNo}`}>{memberDetailObj.fullName}
+              <picture title="Show boards member belongs to">
+                <img style={rotateOnToggle} src={openMemberDetailIcon} alt="show member boards icon" />
+              </picture>
+            </p>
+            <p id={`username${props.indexNo}`}>@{memberDetailObj.username}</p>
 
+            <ul style={openBoardsListStyle}>
+              <h3>Boards member belongs to:</h3>
+
+              {allBoardMemberBelongsArray.length > 0 && (
+                Array.from(allBoardMemberBelongsArray).map((boardName, index) => {
+                  return <li key={index}>{index + 1}. {boardName}</li>;
+                })
+              )}
+            </ul>
+          </article>
+        </section>
+      </form>
+    )
+  );
 }
