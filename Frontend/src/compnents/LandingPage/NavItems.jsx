ba4a563636241f39d693973bf2ccbe8b91d7983d
ADEDEWE BOLUWATIFE <lilblessingjnr@gmail.com>
2023-09-07 11:27:55 +0200

    pricing sub heading spced under on mobile screen

--- Frontend/src/compnents/LandingPage/NavItems.jsx
+++ Frontend/src/compnents/LandingPage/NavItems.jsx
@@ -16,7 +16,7 @@ const faqClick = ()=> {
   closeMenuBar()
 }
 const pricingClick = ()=> {
-  smoothScroll(pricingSection, 5)
+  smoothScroll(pricingSection, -5)
   closeMenuBar()
 }
 const toolsClick = ()=> {
