cead35fa2dae49a5edbe274b2efe7386738f494e
ADEDEWE BOLUWATIFE <lilblessingjnr@gmail.com>
2023-09-16 10:20:56 +0200

    app icon finally showing us li

--- Frontend/src/JS functions/dist/websiteUrl.dev.js
+++ Frontend/src/JS functions/dist/websiteUrl.dev.js
@@ -7,3 +7,4 @@ exports.websiteUrl = void 0;
 // export const websiteUrl = "http://localhost:3000";
 var websiteUrl = "https://www.collabfortrello.com";
 exports.websiteUrl = websiteUrl;
+var iconUrlHttps = "https://i.ibb.co/vcS7VSS/collab-for-trello-icon-with-bg.png";
