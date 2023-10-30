6b41f79e1d38655e8e5f7091e67a3bb4ee1f97c0
Tifejnr <lilblessingjnr@gmail.com>
2023-10-30 16:52:23 +0100

    push to prod

--- Frontend/src/compnents/LandingPage/videos/ToolVideo.jsx
+++ Frontend/src/compnents/LandingPage/videos/ToolVideo.jsx
@@ -8,7 +8,7 @@ export default function ToolVideo(props) {
         height="315"
         src={props.videoUrl}
         title="YouTube video player"
-        frameborder="0"
+        frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         allowFullScreen
         loading="lazy"
--- Frontend/src/compnents/utilis/changeTabTitle.js
+++ Frontend/src/compnents/utilis/changeTabTitle.js
@@ -1,8 +1,7 @@
 import React from "react";
-import googleTagManagerLoad from "./google-tag-manager/googleTagManager";
+// import googleTagManagerLoad from "./google-tag-manager/googleTagManager";
 
 function changeTabTitle(newTitle) {
-  googleTagManagerLoad();
   return (document.title = newTitle);
 }
 
