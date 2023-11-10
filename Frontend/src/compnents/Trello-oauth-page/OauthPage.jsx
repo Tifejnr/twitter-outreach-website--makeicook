b2bd991b227c3c4b6eb99ef523dd841b2725b422
Tifejnr <lilblessingjnr@gmail.com>
2023-11-10 18:53:40 +0100

    authorize button chamed to lowercase

--- Frontend/src/compnents/Trello-oauth-page/OauthPage.jsx
+++ Frontend/src/compnents/Trello-oauth-page/OauthPage.jsx
@@ -1,55 +1,52 @@
+import NavLogo from "../Main-nav-bar/NavLogo";
+import GetStartedIcon from "./GetStartedIcon";
+import forwardArrow from "../../assets/SVGs/thin-long-arrow-right-icon.svg";
+import { changeTabTitle } from "../utilis/changeTabTitle";
+import AuthNav from "../Auth/AuthNav";
+import { IconsObj } from "../utilis/Icons/iconsObj";
 
-import NavLogo from "../Main-nav-bar/NavLogo"
-import GetStartedIcon from "./GetStartedIcon"
-import forwardArrow from "../../assets/SVGs/thin-long-arrow-right-icon.svg"
-import { changeTabTitle } from "../utilis/changeTabTitle"
-import AuthNav from "../Auth/AuthNav"
-import { IconsObj } from "../utilis/Icons/iconsObj"
-
-const oauthPageTabTitle= "Authorize account – Collab for Trello"
-const buttonLabel= "CLICK TO AUTHORIZE"
-
+const oauthPageTabTitle = "Authorize account – Collab for Trello";
+const buttonLabel = "Click to Authorize";
 
 export default function OauthPage() {
-
-  changeTabTitle(oauthPageTabTitle)
+  changeTabTitle(oauthPageTabTitle);
 
   return (
-   <>
+    <>
+      <AuthNav />
 
-   <AuthNav />
+      <main className="auth-page-main-cont">
+        <h1>AUTHORIZE ACCESS</h1>
+        <h5>
+          <b>5</b> free credits for trial
+        </h5>
 
-  <main className="auth-page-main-cont">
-       <h1>AUTHORIZE ACCESS</h1>
-       <h5><b>5</b> free credits for trial</h5>
-
-       <section className="authorize-image-desc">
+        <section className="authorize-image-desc">
           <article>
-            <img src= {IconsObj.trelloIcon} className='trello-icon' />
-              <h3>Trello</h3>
+            <img src={IconsObj.trelloIcon} className="trello-icon" />
+            <h3>Trello</h3>
           </article>
 
-          <picture id="linking-arrow-cont" >
-              <img src={forwardArrow} alt="" />
+          <picture id="linking-arrow-cont">
+            <img src={forwardArrow} alt="" />
           </picture>
 
           <picture>
-            <NavLogo/>
+            <NavLogo />
           </picture>
+        </section>
 
-       </section>
+        <section>
+          <p>
+            To get started, please click the button below to connect Collab for
+            Trello with Trello in order to perform automations on your behalf.
+          </p>
+        </section>
 
-       <section>
-        <p>
-         To get started, please click the button below to connect Collab for Trello with Trello in order to perform automations on your behalf. 
-        </p>
-       </section>
-
-       <section>
-         <GetStartedIcon buttonLabel={buttonLabel}/>
-       </section>
-
-  </main> 
-   </>
-  )
+        <section>
+          <GetStartedIcon buttonLabel={buttonLabel} />
+        </section>
+      </main>
+    </>
+  );
 }
