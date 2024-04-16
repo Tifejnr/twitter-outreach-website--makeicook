4dcbe890b9ed2ffa58b7eb26201a6133c8cf6302
Tifejnr <lilblessingjnr@gmail.com>
2024-04-16 10:26:28 +0100

    push

--- Server/routes/(ai-related)/get-client-name/utils/removeAndTextFromClienName.js
+++ Server/routes/(ai-related)/get-client-name/utils/removeAndTextFromClienName.js
@@ -1,4 +1,4 @@
-function removeAndTextFromClientName(phrase) {
+export default function removeAndTextFromClientName(phrase) {
   // phrase.trim();
   let phraseAndRemoved = phrase.replace(/\band\b|\b&\b/g, "");
   return phraseAndRemoved;
