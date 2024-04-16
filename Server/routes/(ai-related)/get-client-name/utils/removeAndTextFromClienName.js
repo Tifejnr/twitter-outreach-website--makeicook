0f88c03846704dddae8b7a3510f768e6783adade
Tifejnr <lilblessingjnr@gmail.com>
2024-04-16 10:15:26 +0100

    push to prod

--- Server/routes/(ai-related)/get-client-name/utils/removeAndTextFromClienName.js
+++ Server/routes/(ai-related)/get-client-name/utils/removeAndTextFromClienName.js
@@ -1,9 +1,7 @@
-export default function removeAndTextFromClienName(phrase) {
+export default function removeAndTextFromClientName(phrase) {
   // Use a regular expression to replace " and " with a single space
   // The \b ensures that "and" is matched only as a whole word
   // The g flag ensures that all occurrences of " and " are replaced
-  let phraseAndRemoved = phrase.replace(/\band\b/g, "");
-  phraseAndRemoved = phrase.replace(/\b&\b/g, "");
-
+  let phraseAndRemoved = phrase.replace(/\band\b|\b&\b/g, "");
   return phraseAndRemoved;
 }
