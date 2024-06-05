ca1cf68b8a74c0e7165a75d1a9b82f01c96c7e85
Tifejnr <lilblessingjnr@gmail.com>
2024-06-06 01:46:55 +0200

    puh to pof

--- Server/routes/(ai-related)/get-client-name/route.js
+++ Server/routes/(ai-related)/get-client-name/route.js
@@ -104,6 +104,7 @@ getClientNameRouter.post("/", async (req, res) => {
     if (
       firstCleanedClientName !== "Hi there" ||
       firstClientNameResponseRaw !== "." ||
+      !firstClientNameResponseRaw.includes("ignore") ||
       firstCleanedClientName !== "client name is greater than 4"
     )
       return res.json({ clientNameResponse: firstCleanedClientName });
@@ -125,6 +126,7 @@ getClientNameRouter.post("/", async (req, res) => {
     if (
       secondCleanedClientName !== "Hi there" ||
       secondClientNameResponseRaw !== "." ||
+      !secondClientNameResponseRaw.includes("ignore") ||
       secondCleanedClientName !== "client name is greater than 4"
     )
       return res.json({ clientNameResponse: secondCleanedClientName });
