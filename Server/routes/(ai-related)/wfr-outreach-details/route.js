cc9936ea6c7a6457466149017b0a599efb4d54ea
Tifejnr <lilblessingjnr@gmail.com>
2024-06-21 01:39:31 +0100

    add

--- Server/routes/(ai-related)/wfr-outreach-details/route.js
+++ Server/routes/(ai-related)/wfr-outreach-details/route.js
@@ -15,8 +15,6 @@ const wfrOutreachRecordingRouter = express.Router();
 wfrOutreachRecordingRouter.post("/", async (req, res) => {
   const bodyRequest = await req.body;
 
-  console.log("adding username", bodyRequest);
-
   // const resultOfTokenValidation = await isTokenValid(bodyRequest);
 
   // if (resultOfTokenValidation.nullJWTToken)
