ad24c27f3721ab749b678318941c6f2ebc7d8c1d
Tifejnr <lilblessingjnr@gmail.com>
2024-06-27 17:54:02 +0100

    push to prod

--- Server/server-utils/middleware/is-origin-valid/allowedOriginArray.js
+++ Server/server-utils/middleware/is-origin-valid/allowedOriginArray.js
@@ -2,6 +2,12 @@ const originalWebsiteOrigin = "https://workforreputation.com";
 const wfrToolkitExtensionOrigin =
   "chrome-extension://chpmkkhcpfhjdkkeiggiicfejnkhcidb";
 
-const allowedOriginArrays = [originalWebsiteOrigin, wfrToolkitExtensionOrigin];
+const upworkOrigin = "https://www.upwork.com";
+
+const allowedOriginArrays = [
+  originalWebsiteOrigin,
+  wfrToolkitExtensionOrigin,
+  upworkOrigin,
+];
 
 export default allowedOriginArrays;
