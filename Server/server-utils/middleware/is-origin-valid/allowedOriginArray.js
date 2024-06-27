e7878e8863d2c39994197b9653a7e31327f02b19
Tifejnr <lilblessingjnr@gmail.com>
2024-06-27 23:47:44 +0100

    dev mode extension origin added

--- Server/server-utils/middleware/is-origin-valid/allowedOriginArray.js
+++ Server/server-utils/middleware/is-origin-valid/allowedOriginArray.js
@@ -3,12 +3,16 @@ const originalWebsiteOrigin = "https://workforreputation.com";
 const wfrToolkitExtensionOrigin =
   "chrome-extension://chpmkkhcpfhjdkkeiggiicfejnkhcidb";
 
+const wfrExtensionDevModeOrigin =
+  "chrome-extension://mpmnfefpnplkkhoegfdfpfpgicpkhgbb";
+
 const upworkOrigin = "https://www.upwork.com";
 const twitterOrigin = "https://twitter.com";
 
 const allowedOriginArrays = [
   originalWebsiteOrigin,
   wfrToolkitExtensionOrigin,
+  wfrExtensionDevModeOrigin,
   upworkOrigin,
   twitterOrigin,
 ];
