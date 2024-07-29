51f4ee6df598875aa51a5a31622b9ce99f44435d
Tifejnr <lilblessingjnr@gmail.com>
2024-07-29 09:30:23 +0100

    bab abab

--- Frontend/src/components/auth/utils/images/allImagesContainer.js
+++ Frontend/src/components/auth/utils/images/allImagesContainer.js
@@ -1,4 +1,4 @@
-import taFeedback from "../../../../assets/images/wfr-feedbacks/TA feedback.png";
+import taFeedback from "../../../../assets/images/wfr-feedbacks/temidayo-feedback-wfr.jpg";
 import abolodeFeedback from "../../../../assets/images/wfr-feedbacks/bean-writer-wfr-review.jpg";
 import chiefBasiFeedback from "../../../../assets/images/wfr-feedbacks/basic-chrief-feedback.png";
 import mightyFeedback from "../../../../assets/images/wfr-feedbacks/email-beast-feedback.jpg";
