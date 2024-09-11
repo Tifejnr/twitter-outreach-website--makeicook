b41d78f4e5dbae3cca05a63755fafeae1f2b169d
Tifejnr <lilblessingjnr@gmail.com>
2024-09-11 21:58:22 +0100

    push to prod

--- Server/routes/(ai-related)/get-client-name/route.js
+++ Server/routes/(ai-related)/get-client-name/route.js
@@ -15,9 +15,11 @@ const hf = new HfInference(HF_TOKEN);
 
 const getClientNamePromptHeading = `Return only all the human names in the texts below, only return the human names, don't prefix your response with text at all. 
    
-   if there are more than two distinct names, return "Multiple names, Hi".
+   if there are more than two differently distinct names, return "Multiple names, Hi".
    
    If there are no human names in the text below, return "Hi there".
+
+  if there are more than two differently distinct names, return "Multiple names, Hi".
 
    Text below : 
 `;
