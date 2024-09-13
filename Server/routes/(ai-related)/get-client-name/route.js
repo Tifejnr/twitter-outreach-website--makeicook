f46b667a31f62c862f6d8da5b9f8d709ef1f6828
Tifejnr <lilblessingjnr@gmail.com>
2024-09-13 17:31:41 -0500

    push to prod

--- Server/routes/(ai-related)/get-client-name/route.js
+++ Server/routes/(ai-related)/get-client-name/route.js
@@ -71,8 +71,6 @@ const editFirstNamePromptInstruction = `
 
   Don't count any phrase including personal pronouns " he" , "She", "him" , "her", "his" as human name.
 
- If you ever come across a name called "Will", check for the context surrounding the name "Will" before choosing it as human name, check this well.
-  
   if there are multiple names, seperate them using comma.
 
   Never repeat the same name.
