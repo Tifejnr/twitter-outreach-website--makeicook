c374e1a48fa8d25c3dd454f6007dff4561febb54
Tifejnr <lilblessingjnr@gmail.com>
2024-09-13 16:36:30 +0100

    push to prod

--- Server/routes/(ai-related)/get-client-name/route.js
+++ Server/routes/(ai-related)/get-client-name/route.js
@@ -284,19 +284,20 @@ function findCommonName(names) {
 
   // Find the shortest name in the array and remove both commas and hyphens
   let shortest = namesArray.reduce((a, b) => (a.length <= b.length ? a : b));
+  let longestName = namesArray.reduce((a, b) => (a.length >= b.length ? a : b));
 
   const namesArrayLength = namesArray.length;
 
   for (let i = 0; i < namesArray.length; i++) {
-    const currentName = namesArray[i];
+    const currentName = namesArray[i].toLowerCase();
 
-    if (currentName.includes(shortest)) {
+    if (currentName.includes(shortest.toLowerCase())) {
       passedArray.push(1);
     }
   }
 
   if (passedArray.length == namesArrayLength) {
-    return shortest;
+    return longestName;
   }
   return names;
 }
