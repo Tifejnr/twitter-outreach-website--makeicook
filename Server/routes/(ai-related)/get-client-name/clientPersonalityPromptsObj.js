a7646ac762c5a166abd7ca1e4ec2a8871e763a96
Tifejnr <lilblessingjnr@gmail.com>
2024-09-16 09:56:30 +0100

    push to prod

--- Server/routes/(ai-related)/get-client-name/clientPersonalityPromptsObj.js
+++ Server/routes/(ai-related)/get-client-name/clientPersonalityPromptsObj.js
@@ -1,7 +1,7 @@
 const promptToActForClientSummary = `
 The text below is what frelancers said about a client.
 
-In a sentence, less than 50 words, summarise how working with the client is:
+In a sentence, less than 70 words, summarise how working with the client is:
 
 Start your response with : "Here is a summary of how freelancers described working with the client:"
 
