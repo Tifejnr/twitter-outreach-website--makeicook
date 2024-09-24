4f4323bdacfde7ab7f7a6da894be1823e9ddfe28
Tifejnr <lilblessingjnr@gmail.com>
2024-09-24 22:14:21 +0100

    push to prod

--- Server/routes/(ai-related)/get-client-name/route.js
+++ Server/routes/(ai-related)/get-client-name/route.js
@@ -209,7 +209,7 @@ getClientNameRouter.post("/", async (req, res) => {
       prompt
     );
 
-    if (isFinalNamePresentInFeedback == "No") {
+    if (isFinalNamePresentInFeedback == "No" && !finalName.includes(",")) {
       return res.json({
         clientNameResponse: realNoNamesFoundResponse,
         clientPersonality,
