32f49fe8a968283403ac5d8bfdc77c8d337bcf55
Tifejnr <lilblessingjnr@gmail.com>
2024-10-08 10:07:48 +0100

    pushbto prod

--- Server/routes/(ai-related)/get-client-name/utils/are-all-names-forbidden/areAllNamesForbidden.js
+++ Server/routes/(ai-related)/get-client-name/utils/are-all-names-forbidden/areAllNamesForbidden.js
@@ -30,70 +30,67 @@ export default function areAllNamesForbidden(finalName) {
   }
 }
 
-`
-On the job search page, click on the job you would like to apply for to view its details.
+// `
+// On the job search page, click on the job you would like to apply for to view its details.
 
-Click the "Apply Now" button.
+// Click the "Apply Now" button.
 
-Above the cover letter input box, you will see a new layout. The last button on the right contains the client’s name, which is automatically fetched.
+// Above the cover letter input box, you will see a new layout. The last button on the right contains the client’s name, which is automatically fetched.
 
-Click the button to copy the name and use it to personalize your cover letter.
+// Click the button to copy the name and use it to personalize your cover letter.
 
-In cases where multiple names are found, indicating that the client account is managed by more than one person, the client name is prefixed by "Names -" followed by a list of the names found.
+// In cases where multiple names are found, indicating that the client account is managed by more than one person, the client name is prefixed by "Names -" followed by a list of the names found.
 
-It's up to you to decide whether to use one of the names or simply address the client with "Hi."
+// It's up to you to decide whether to use one of the names or simply address the client with "Hi."
 
-In scenarios where no name is found, "No name was found" is displayed. In such cases, use "Hi" for personalization.
+// In scenarios where no name is found, "No name was found" is displayed. In such cases, use "Hi" for personalization.
 
-Additionally, if the client has no job history or no text-based feedback in their job history, "client has no job history" is displayed. Use "Hi" for personalization in this situation too.
+// Additionally, if the client has no job history or no text-based feedback in their job history, "client has no job history" is displayed. Use "Hi" for personalization in this situation too.
 
-Note: Credits are only deducted each time you access the proposal submission page.
-``
-First of all, pin the WFR Toolkit extension to your toolbar.
+// Note: Credits are only deducted each time you access the proposal submission page.
+// ``
+// First of all, pin the WFR Toolkit extension to your toolbar.
 
-To do this, locate the extensions icon on the toolbar
+// To do this, locate the extensions icon on the toolbar
 
-Click the the extensions icon
+// Click the the extensions icon
 
-find WFR Toolkit extensions from the extensions list.
+// find WFR Toolkit extensions from the extensions list.
 
-Click the pin button to pin it to your toolbar.
+// Click the pin button to pin it to your toolbar.
 
-You should see it pinned to your toolbar now.
+// You should see it pinned to your toolbar now.
 
-Click the WFR Toolkit icon on your toolbar to go to the extension homepage.
+// Click the WFR Toolkit icon on your toolbar to go to the extension homepage.
 
-On the left side of the extension homepage, locate "Text template" on the sections list.
+// On the left side of the extension homepage, locate "Text template" on the sections list.
 
-Click the text templates list to naivagte to Text templates section.
+// Click the text templates list to naivagte to Text templates section.
 
+// On the text template section, locate "Create new text template" button.
 
-On the text template section, locate "Create new text template" button.
+// click "Create new text template" button to create new template.
 
-click "Create new text template" button to create new template.
+// Enter your template name.
 
-Enter your template name.
+// Locate the text input box.
 
-Locate the text input box.
+// Paste your text into the input box.
 
-Paste your text into the input box.
+// Locate and click the "Save template" button.
 
-Locate and click the "Save template" button. 
+// Locate and click the "Go back" button.
 
+// Close the tab.
 
-Locate and click the "Go back" button.
+// Try to apply to any job so you can access the proposal submission page where you will be able to use the text templates.
 
-Close the tab.
+// Locate "CTT" button by the far right of the cover letter input box.
 
-Try to apply to any job so you can access the proposal submission page where you will be able to use the text templates.
+// Click the CTT button to display a popover showing all saved text templates.
 
-Locate "CTT" button by the far right of the cover letter input box.
+// Locate the name of the text template you will like to copy and click "copy".
 
-Click the CTT button to display a popover showing all saved text templates.
+// Paste the copied text.
 
-Locate the name of the text template you will like to copy and click "copy".
-
-
-Paste the copied text.
-
-`;
+// `;
