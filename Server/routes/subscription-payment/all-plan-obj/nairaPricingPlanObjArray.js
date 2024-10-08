42f1ccb9c71bb31eeea0b46dae87a1a5c9d802d5
Tifejnr <lilblessingjnr@gmail.com>
2024-10-08 12:08:17 +0100

    push to prod

--- Server/routes/subscription-payment/all-plan-obj/nairaPricingPlanObjArray.js
+++ Server/routes/subscription-payment/all-plan-obj/nairaPricingPlanObjArray.js
@@ -13,7 +13,7 @@ const basicPlanObj = {
   planName: "Standard",
   perCreditInfo: 10,
   planCreditsAmount: 3000,
-  planPrice: 4900,
+  planPrice: 500,
   suitabilityTimeUsage: "less than 10",
   naira: true,
   noOfDailyJobsItCanBeUsedToApplyTo: 10,
