9e535517d45699bdb0f2bd2f6a4885a64b4946e1
Tifejnr <lilblessingjnr@gmail.com>
2024-10-13 15:46:35 +0100

    push to prod

--- Server/server-utils/joi-validations/sign-up/extensionSignUpValidation.js
+++ Server/server-utils/joi-validations/sign-up/extensionSignUpValidation.js
@@ -5,6 +5,7 @@ export default function extensionSignUpValidation(reqBody) {
     name: Joi.string().min(3).max(150).required(),
     email: Joi.string().min(3).max(250).required().email(),
     password: Joi.string().min(3).max(250).required(),
+    entryCode: Joi.string().min(0).max(20),
     fromExtension: Joi.boolean().required(),
   });
 
