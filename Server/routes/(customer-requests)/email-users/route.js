26e6210b3a4a9a6ef866fe0bc8d33158e9848d5f
Tifejnr <lilblessingjnr@gmail.com>
2024-10-14 15:12:19 +0100

    push to prod

--- Server/routes/(customer-requests)/email-users/route.js
+++ Server/routes/(customer-requests)/email-users/route.js
@@ -21,12 +21,12 @@ async function sendMailToMultipleUsers() {
 
   // console.log("lastRegisteredUser", lastRegisteredUser);
 
-  const currentUserNow = {
-    email: "akovahh@gmail.com",
-    name: "Adedewe Boluwatife",
-  };
+  // const currentUserNow = {
+  //   email: "akovahh@gmail.com",
+  //   name: "Adedewe Boluwatife",
+  // };
 
-  sendMailFunction(currentUserNow);
+  // sendMailFunction(currentUserNow);
 
   // return;
 
@@ -50,8 +50,8 @@ async function sendMailToMultipleUsers() {
   //     allUsersThatHaveUsedExtensionOnce.length - 1
   //   ]
   // );
-  // console.log("start sending jajaj");
-  // runWithDelay(allUsersThatHaveUsedExtensionOnce, sendMailFunction);
+  console.log("start sending jajaj");
+  runWithDelay(allUsersThatHaveUsedExtensionOnce, sendMailFunction);
 }
 
 async function sendMailFunction(user) {
