08626e88c560250dfe911dfc9ea2bb87d51636e0
Tifejnr <lilblessingjnr@gmail.com>
2024-10-20 14:18:29 -0500

    push to rpdo

--- Server/routes/subscription-payment/webhook/webhookLemonsqueezy.js
+++ Server/routes/subscription-payment/webhook/webhookLemonsqueezy.js
@@ -15,17 +15,15 @@ const secret = keysObjects.webHookSecretLemon;
 const orderCreatedEvent = "order_created";
 
 const webhookLemonsqueezyRouter = express.Router();
-
 // Custom middleware to capture the raw request body before parsing it
 webhookLemonsqueezyRouter.use(
   bodyParser.json({
     verify: (req, res, buf) => {
-      req.rawBody = buf.toString();
+      req.rawBody = buf; // Keep this as a buffer
     },
   })
 );
 
-// Endpoint to handle incoming webhook events
 webhookLemonsqueezyRouter.post("/", async (req, res) => {
   console.log("req.body", req.body);
   try {
@@ -34,17 +32,17 @@ webhookLemonsqueezyRouter.post("/", async (req, res) => {
     // Verify the signature
     const hmac = crypto.createHmac("sha256", secret);
     const generatedSigFromBody = Buffer.from(
-      hmac.update(req.body).digest("hex"),
+      hmac.update(req.rawBody).digest("hex"), // Use req.rawBody instead of req.body
       "utf8"
     );
 
     if (!crypto.timingSafeEqual(generatedSigFromBody, headerSignarture))
       return res.status(403).json({ error: "Invalid signature." });
-    // Signature is valid, process the webhook event
 
+    // Signature is valid, process the webhook event
     console.log("eeeee reach ooo", req.body);
 
-    //destructuring data sent to get needed details
+    // Process the event...
     const { data, meta } = req.body;
     const { event_name, custom_data } = meta;
     const { user_id } = custom_data;
@@ -53,36 +51,26 @@ webhookLemonsqueezyRouter.post("/", async (req, res) => {
       const accountUser = await user.findById(user_id);
       if (!accountUser) return res.status(400).json({ invalid_User: true });
 
-      //destructuring data sent to get payment details
       const { attributes } = data;
       const { first_order_item, status_formatted } = attributes;
       const { variant_id } = first_order_item;
-      console.log(status_formatted);
 
       if (status_formatted != "Paid") return res.sendStatus(204);
-      //getting product details to update
+
       const product = allPlansArrayObj.find(
         (planObj) => planObj.variantId == variant_id
       );
 
       if (!product) {
-        console.log("product not found");
         return res.status(402).json({ notFound: true });
       }
-      //set  user status to paid
+
       accountUser.isPaid = true;
+      accountUser.credits += product.credits;
 
-      //add the payed for credit to current users credits
-      const currentUserCredit = accountUser.credits;
-      accountUser.credits = currentUserCredit + product.credits;
-
-      //save user details
       await accountUser.save();
-
-      // Respond with a 200 status to acknowledge receipt of the webhook
       return res.sendStatus(200);
     } else {
-      // For other events, respond with a 204 status to indicate that the event is not handled by this endpoint
       return res.sendStatus(204);
     }
   } catch (error) {
