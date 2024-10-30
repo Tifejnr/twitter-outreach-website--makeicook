c9095b6931b41a86bd959efa01888fb7e9267dd3
Tifejnr <lilblessingjnr@gmail.com>
2024-10-30 23:45:35 +0000

    push to prod

--- Server/routes/(ai-related)/wfr-outreach-details/route.js
+++ Server/routes/(ai-related)/wfr-outreach-details/route.js
@@ -1,8 +1,8 @@
 import { google } from "googleapis";
 import express from "express";
 const sheets = google.sheets("v4");
-// import credentials from "./credentials.json" assert { type: "json" }; // Replace with the actual path to your credentials file
-const credentials = "hi";
+import credentials from "./credentials.json" assert { type: "json" }; // Replace with the actual path to your credentials file
+// const credentials = "hi";
 // Set up OAuth2 authentication
 const auth = new google.auth.GoogleAuth({
   credentials,
