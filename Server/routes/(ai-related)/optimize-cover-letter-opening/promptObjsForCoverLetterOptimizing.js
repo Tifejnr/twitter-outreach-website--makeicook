035e0d3179b09a196808d7c7db657bb6c7308635
Tifejnr <lilblessingjnr@gmail.com>
2024-10-31 15:03:32 +0000

    push to prod

--- Server/routes/(ai-related)/optimize-cover-letter-opening/promptObjsForCoverLetterOptimizing.js
+++ Server/routes/(ai-related)/optimize-cover-letter-opening/promptObjsForCoverLetterOptimizing.js
@@ -2,7 +2,9 @@ const describeClientPainPoints = `
 describe all the client pain points from this job description.
 `;
 
-const reduceTo250xters = `Reduce this cover letter opening to 250 characters without losing its structure 
+const reduceTo250xters = `Reduce this cover letter opening line to 250 characters without losing its structure. 
+
+Make it super simple that it is understandable to a 5 year old boy. 
 
 Only return the reduced cover letter opening. don't explain anything, don't prefix the main cover letter with any explanantion or any revision.
 `;
