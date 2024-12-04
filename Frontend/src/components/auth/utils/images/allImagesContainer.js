75449e50113116a919427d3c496ddce894804b60
Tifejnr <lilblessingjnr@gmail.com>
2024-12-04 19:35:46 +0100

    push to prod

--- Frontend/src/components/auth/utils/images/allImagesContainer.js
+++ Frontend/src/components/auth/utils/images/allImagesContainer.js
@@ -1,17 +1,17 @@
-import taFeedback from "../../../../assets/images/wfr-feedbacks/temidayo-feedback-wfr.jpg";
-import abolodeFeedback from "../../../../assets/images/wfr-feedbacks/zahaab-wfr-testimonial.jpg";
-import chiefBasiFeedback from "../../../../assets/images/wfr-feedbacks/basic-chrief-feedback.png";
-import mightyFeedback from "../../../../assets/images/wfr-feedbacks/email-beast-feedback.jpg";
-import prakbarFeedback from "../../../../assets/images/wfr-feedbacks/richard-wfr-feedback.jpg";
-import tobFeedback from "../../../../assets/images/wfr-feedbacks/kunal-koli-wfr-review.jpg";
+// import taFeedback from "../../../../assets/images/wfr-feedbacks/temidayo-feedback-wfr.jpg";
+// import abolodeFeedback from "../../../../assets/images/wfr-feedbacks/zahaab-wfr-testimonial.jpg";
+// import chiefBasiFeedback from "../../../../assets/images/wfr-feedbacks/basic-chrief-feedback.png";
+// import mightyFeedback from "../../../../assets/images/wfr-feedbacks/email-beast-feedback.jpg";
+// import prakbarFeedback from "../../../../assets/images/wfr-feedbacks/richard-wfr-feedback.jpg";
+// import tobFeedback from "../../../../assets/images/wfr-feedbacks/kunal-koli-wfr-review.jpg";
 
-const allImagesContainerArray = [
-  tobFeedback,
-  abolodeFeedback,
-  taFeedback,
-  chiefBasiFeedback,
-  mightyFeedback,
-  prakbarFeedback,
-];
+// const allImagesContainerArray = [
+//   tobFeedback,
+//   abolodeFeedback,
+//   taFeedback,
+//   chiefBasiFeedback,
+//   mightyFeedback,
+//   prakbarFeedback,
+// ];
 
-export default allImagesContainerArray;
+// export default allImagesContainerArray;
