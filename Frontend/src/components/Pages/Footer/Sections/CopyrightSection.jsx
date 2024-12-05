f53d7f82047a0ed754bbd8f66e245b7e2d58691b
Tifejnr <lilblessingjnr@gmail.com>
2024-12-05 15:25:32 +0100

    push to prod

--- Frontend/src/components/Pages/Footer/Sections/CopyrightSection.jsx
+++ Frontend/src/components/Pages/Footer/Sections/CopyrightSection.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function CopyrightSection() {
-  return <p className="copyright-text">© Work for Reputation 2024</p>;
+  return <p className="copyright-text">© Make I Cook 2024</p>;
 }
