caab526b1daf85cec81d5d3be1990df0830ce8cb
Tifejnr <lilblessingjnr@gmail.com>
2024-12-22 00:25:26 +0100

    push to prod

--- Server/routes/stored-messages-on-server/make-i-cook-tutorials/allMakeICookTutorialsArray.js
+++ Server/routes/stored-messages-on-server/make-i-cook-tutorials/allMakeICookTutorialsArray.js
@@ -2,7 +2,7 @@ import howToDmTutorialObj from "./each-make-cook-tutorial/howToDmTutorialObj.js"
 import addGoogleSheetDetailsObj from "./each-make-cook-tutorial/addGoogleSheetDetailsObj.js";
 
 const allMakeICookTutorialsArray = [
-  //   howToDmTutorialObj,
+  howToDmTutorialObj,
   addGoogleSheetDetailsObj,
 ];
 
--- Server/routes/stored-messages-on-server/make-i-cook-tutorials/each-make-cook-tutorial/howToDmTutorialObj.js
+++ Server/routes/stored-messages-on-server/make-i-cook-tutorials/each-make-cook-tutorial/howToDmTutorialObj.js
@@ -1,5 +1,6 @@
-const heading = `3 minute tutorial - How to start Dming`;
-const videoUrl = "hhhh";
+const heading = `How to start sending Dms with Twitter (X) Dm Outreach extension.`;
+const videoUrl =
+  "https://www.youtube.com/embed/7qd8Z8cTm6w?si=Hc-ckMxqbvKGqcH0";
 
 const howToDmTutorialObj = {
   heading,
