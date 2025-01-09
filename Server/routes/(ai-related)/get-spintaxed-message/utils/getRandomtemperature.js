976bcb9ee1299e301342992ecf4960e0545b09d0
Tifejnr <lilblessingjnr@gmail.com>
2025-01-09 21:39:05 +0100

    push to prod

--- Server/routes/(ai-related)/get-spintaxed-message/utils/getRandomtemperature.js
+++ Server/routes/(ai-related)/get-spintaxed-message/utils/getRandomtemperature.js
@@ -1,4 +1,5 @@
 export default function getRandomTemperature(lastPickedTemperature) {
+  console.log("lastPickedTemperature", lastPickedTemperature);
   let newTemperature;
   do {
     newTemperature = Math.random() * 0.8 + 0.1; // Generates a number between 0.1 and 0.9
@@ -6,5 +7,8 @@ export default function getRandomTemperature(lastPickedTemperature) {
     newTemperature === lastPickedTemperature ||
     Math.abs(newTemperature - lastPickedTemperature) <= 2
   );
+
+  console.log("newTemperature", newTemperature);
+
   return newTemperature;
 }
