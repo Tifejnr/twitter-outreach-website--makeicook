ec2c4c8179b72f1bff7cb72f654c7b7d4f8aace0
Tifejnr <lilblessingjnr@gmail.com>
2025-01-11 02:05:17 +0100

    [ush to prod

--- Server/routes/stored-messages-on-server/make-i-cook-tutorials/each-make-cook-tutorial/howToDmTutorialObj.js
+++ Server/routes/stored-messages-on-server/make-i-cook-tutorials/each-make-cook-tutorial/howToDmTutorialObj.js
@@ -1,6 +1,6 @@
-const heading = `How to start sending Dms with Twitter (X) Prospecting extension.`;
+const heading = `How to start sending Dms with Twitter (X) Prospecting extension. (Leads scraping, filtering and messaging)`;
 const videoUrl =
-  "https://www.youtube.com/embed/7qd8Z8cTm6w?si=Hc-ckMxqbvKGqcH0";
+  "https://www.youtube.com/embed/mpLJvWPGWhY?si=IPg7kUxDGIv2PJUX";
 
 const howToDmTutorialObj = {
   heading,
